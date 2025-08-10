c2449d350078a85ccf461be778b21bacb285d083
cw <jamjam12@ajou.ac.kr>
2025-08-10 21:36:02 +0900

    [Silver III] Title: N과 M (1), Time: 164 ms, Memory: 31800 KB -BaekjoonHub

--- 백준/Silver/15649. N과 M （1）/N과 M （1）.java
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.java
@@ -1,50 +1,45 @@
+import java.util.*;
 import java.io.*;
-import java.util.ArrayList;
-import java.util.List;
 
-public class Main {
-
-    static boolean visited[];
-    static int b;
-    static List<String> target = new ArrayList<>();
-
-    public static void main(String[] args) throws IOException{
-        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-
-        String[] arr = bf.readLine().split(" ");
-        int a = Integer.parseInt(arr[0]);
-        b = Integer.parseInt(arr[1]);
-
-        visited = new boolean[a];
+public class Main{
+  static int N;
+  static int M;
+  static boolean[] visited;
+  static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
 
-        dfs(0,"");
+  public static void main(String[] args) throws IOException{
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+    N = Integer.parseInt(st.nextToken());
+    M = Integer.parseInt(st.nextToken());
+    visited = new boolean[N+1];
 
-        for(String s : target){
-            bw.write(s+"\n");
-        }
-        bw.flush();
-        bw.close();
+    dfs(0 , "");
+    bw.flush();
+  }
+
+  /*
+  idx : 몇번 인덱스부터 추가하면서 검사를 시킬지
+  depth : 숫자를 몇개 뽑았는지
+   */
+  public static void dfs(int depth , String get) throws IOException {
+    if(depth == M)
+    {
+      bw.write(get+"\n");
+      return;
     }
-    public static void dfs(int cnt, String s){
-        //b(순열을 뽑아야 하는 횟수와 카운트 횟수가 같으면 집어넣고 중지)
-        if(cnt == b){
-            target.add(s);
-            return;
-        }
 
-        //아니라면 재귀 호출, 카운트 횟수를 올려서 매개변수로 넣어주기
-        for(int j = 0; j < visited.length; j++){
-            //안되면 들어가면 안돼!!!!되는 것만 재귀를 호출해야 한다.
-            if(!visited[j])
-            {
-                visited[j] = true;
-                if (s.length()==0) dfs(cnt+1,s+(j+1));
-                else dfs(cnt+1,s+" "+(j+1));
-                visited[j] = false;
-            }
+    for(int i = 1; i <= N; i++)
+    {
+      if(!visited[i])
+      {
+        visited[i] = true;
+        dfs(depth+1, get+i+" ");
+        visited[i] = false;
+      }
 
-        }
     }
+
+  }
 }
