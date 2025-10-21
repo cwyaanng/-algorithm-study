242c01edf53510c048cb5dea0d848fe639b08b30
cw <jamjam12@ajou.ac.kr>
2025-10-21 23:09:31 +0900

    [D2] Title: 스도쿠 검증, Time: 117 ms, Memory: 29,312 KB -BaekjoonHub

---
+++ SWEA/D2/1974. 스도쿠 검증/스도쿠 검증.java
@@ -0,0 +1,106 @@
+/////////////////////////////////////////////////////////////////////////////////////////////
+// 기본 제공코드는 임의 수정해도 관계 없습니다. 단, 입출력 포맷 주의
+// 아래 표준 입출력 예제 필요시 참고하세요.
+// 표준 입력 예제
+// int a;
+// double b;
+// char g;
+// String var;
+// long AB;
+// a = sc.nextInt();                           // int 변수 1개 입력받는 예제
+// b = sc.nextDouble();                        // double 변수 1개 입력받는 예제
+// g = sc.nextByte();                          // char 변수 1개 입력받는 예제
+// var = sc.next();                            // 문자열 1개 입력받는 예제
+// AB = sc.nextLong();                         // long 변수 1개 입력받는 예제
+/////////////////////////////////////////////////////////////////////////////////////////////
+// 표준 출력 예제
+// int a = 0;                            
+// double b = 1.0;               
+// char g = 'b';
+// String var = "ABCDEFG";
+// long AB = 12345678901234567L;
+//System.out.println(a);                       // int 변수 1개 출력하는 예제
+//System.out.println(b); 		       						 // double 변수 1개 출력하는 예제
+//System.out.println(g);		       						 // char 변수 1개 출력하는 예제
+//System.out.println(var);		       				   // 문자열 1개 출력하는 예제
+//System.out.println(AB);		       				     // long 변수 1개 출력하는 예제
+/////////////////////////////////////////////////////////////////////////////////////////////
+import java.util.Scanner;
+import java.io.FileInputStream;
+import java.util.*;
+
+/*
+   사용하는 클래스명이 Solution 이어야 하므로, 가급적 Solution.java 를 사용할 것을 권장합니다.
+   이러한 상황에서도 동일하게 java Solution 명령으로 프로그램을 수행해볼 수 있습니다.
+ */
+class Solution
+{
+    public static int[][] table; 
+    
+	public static void main(String args[]) throws Exception
+	{
+		Scanner sc = new Scanner(System.in);
+		int T;
+		T=sc.nextInt();
+		/*
+		   여러 개의 테스트 케이스가 주어지므로, 각각을 처리합니다.
+		*/
+
+		for(int test_case = 1; test_case <= T; test_case++)
+		{
+		
+			/////////////////////////////////////////////////////////////////////////////////////////////
+            table = new int[9][9];
+			for(int i = 0; i < 9; i++)
+            {
+                for(int j = 0; j <9; j++)
+                {
+                    table[i][j] = sc.nextInt();
+                }
+            }
+            boolean flag = check();
+            if(flag) System.out.printf("#%d 1\n", test_case);
+            else System.out.printf("#%d 0\n",test_case);
+            
+            
+			/////////////////////////////////////////////////////////////////////////////////////////////
+
+		}
+	}
+    
+    public static boolean check(){
+        for(int i = 0; i < 9; i++)
+        {
+            HashMap<Integer,Integer> map = new HashMap<>(); 
+            for(int j = 0; j<9; j++)
+            {
+               if(!map.containsKey(table[i][j])) map.put(table[i][j] , 1);
+               else return false; 
+            }
+            
+            map = new HashMap<>(); 
+            for(int j = 0; j <9; j++)
+            {
+                if(!map.containsKey(table[j][i])) map.put(table[j][i] , 1);
+                else return false; 
+            } 
+        }
+        
+        for(int i = 0; i < 7; i+=3)
+        {
+            for(int j = 0; j < 7; j+=3)
+            {
+                 HashMap<Integer,Integer> map = new HashMap<>(); 
+                for(int k = 0; k < 3; k++)
+                {
+                    for(int r = 0; r < 3; r++)
+                    {
+                        if(!map.containsKey(table[i+k][j+r])) map.put(table[i+k][j+r] , 1);
+                		else return false; 
+                    }   
+                }
+            }
+        }
+        return true; 
+    }
+}
